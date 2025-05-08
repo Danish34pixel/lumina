286f7926ce8607139fa1ae7d242c23711d3766a8
Danish34pixel <danishkhaannn34@gmail.com>
2025-05-08 14:43:44 +0530

    something

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./app.css"; // Adjust the path if your CSS file is located elsewhere
+import "./App.css"; // Adjust the path if your CSS file is located elsewhere
 
 import App from "./App";
 
